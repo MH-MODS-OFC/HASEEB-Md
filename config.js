bb5651b9950fb6207f6452c88aae679ebd72051d
Itxxwasi <142110420+Itxxwasi@users.noreply.github.com>
2024-02-04 22:00:36 +0500

    WASI TECH BOT V1

--- config.js
+++ config.js
@@ -52,7 +52,7 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923184474176,923xxxxxxxx";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923007087913";
 
 
 
